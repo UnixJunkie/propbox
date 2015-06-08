4bd7ffa909cda2312822a598fc7e5bc443c58dd7
Andrew Dalke <dalke@dalkescientific.com>
2015-06-08 18:19:56 +0200

    added batching and a way to install your own resolver

--- propbox/rdprops.py
+++ propbox/rdprops.py
@@ -4,10 +4,12 @@ import sys
 import argparse
 import csv
 import re
+import itertools
 
-from . import rdkit_descriptors
+from . import rdkit_descriptors, nci
+
 from . import rdkit_toolkit as T
-
+from . import Propbox, Resolver
 
 dialects = ["tab", "space", "whitepace"] + csv.list_dialects() 
 
@@ -31,6 +33,9 @@ parser.add_argument("--format", choices=("smi", "sdf", "smi.gz", "sdf.gz"),
 parser.add_argument("--columns", "-P", default="id,smiles,MolWt",
                     help="comma-separated list of descriptors to compute (default: 'id,smiles,MolWt')")
 
+parser.add_argument("--resolver", "-r", default=[], action="append",
+                    help="Add an additional resolver to the current resolver")
+
 parser.add_argument("--headers", "-H",
                     help="comma-separated list of headers (default: use the property names)")
 
@@ -43,10 +48,14 @@ parser.add_argument("--output", "-o",
 parser.add_argument("--dialect", choices=dialects, default="tab",
                     help="CSV output dialect, one of %s (default: 'tab')"
                     % (repr(dialects)[1:-1],))
+
                     
 parser.add_argument("-R", dest="reader_args", type=parse_reader_arg, default=[], action="append",
                     help="add a reader argument")
-                    
+
+parser.add_argument("--batch-size", metavar="N", default="1000",
+                    help="Process N records at a time (default: 1000; use 'all' for all)")
+
 parser.add_argument("filename", nargs="?", default=None,
                     help="input structure file")
 
@@ -81,10 +90,32 @@ parser.add_argument("--list", action="store_true",
 ##         i = j+1
 ##     return format_fields
 
+def _open_output(destination):
+    if destination is None:
+        return sys.stdout, None
+    else:
+        f = open(destination, "w")
+        return f, f.close
+
+def import_resolver(resolver_name):
+    module = __import__(resolver_name, globals(), {}, 0)
+    obj = module
+    for name in resolver_name.split("."):
+        obj = getattr(obj, name)
+    if not isinstance(obj, Resolver):
+        sys.stderr.write("%r does not appear to be a resolver\n" % (resolver_name,))
+    return obj
+    
 def main(argv=None):
     args = parser.parse_args(argv)
 
-    resolver = rdkit_descriptors.resolver
+
+    resolver = Propbox()
+    resolver.add_resolver(rdkit_descriptors.resolver)
+    resolver.add_resolver(nci.resolver)
+
+    for resolver_name in args.resolver:
+        resolver.add_resolver(import_resolver(resolver_name))
 
     known_names = set(resolver.output_names)
     known_names.update(["id", "input_record", "input_format"])
@@ -115,21 +146,45 @@ def main(argv=None):
     format = T.get_input_format_from_source(args.filename, args.format)
     text_settings = dict(args.reader_args)
     reader_args = format.get_reader_args_from_text_settings(text_settings)
-            
-    properties = rdkit_descriptors.make_table_from_source(
-        args.filename,
-        format = format,
-        id_tag = args.id_tag,
-        reader_args = reader_args,
-        )
-    
-    
-    if args.output:
-        properties.save(args.output, columns, headers=headers, dialect=args.dialect,
-                        include_header = not args.no_header)
+
+
+    batch_size = args.batch_size
+    if batch_size == "all":
+        batch_size = None
+    elif not batch_size.isdigit():
+        parser.error("--batch-size must be a positive integer or 'all'")
     else:
-        properties.save(sys.stdout, columns, headers=headers, dialect=args.dialect,
-                        include_header = not args.no_header)
+        batch_size = int(batch_size)
+        if batch_size <= 0:
+            parser.error("--batch-size must be a positive integer or 'all'")
+
+    reader = T.read_ids_and_molecules(
+        args.filename, format, id_tag=args.id_tag, reader_args=reader_args)
+
+
+    outfile, close_outfile = _open_output(args.output)
+    include_header = not args.no_header
+
+    try:
+        while 1:
+            if batch_size is None:
+                batch_reader = reader
+            else:
+                batch_reader = itertools.islice(reader, 0, batch_size)
+            ids_and_mols = list(batch_reader)
+            if not ids_and_mols:
+                break
+
+            properties = rdkit_descriptors.make_table_from_ids_and_molecules(
+                ids_and_mols, resolver=resolver, config=None)
+            properties.save(outfile, columns, headers=headers, dialect=args.dialect,
+                            include_header=include_header)
+
+            # Don't include the header on subsequent writes
+            include_header = False
+    finally:
+        if close_outfile is not None:
+            close_outfile()
                         
     
 if __name__ == "__main__":
